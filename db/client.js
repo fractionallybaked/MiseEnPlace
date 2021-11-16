1cc26d521b44d069af48ad3e7debae29eb9b81f8
Robert Liu <robertpliu@gmail.com>
2021-11-15 20:39:15 -0800

    heroku please??

--- db/client.js
+++ db/client.js
@@ -1,11 +1,18 @@
 // Connect to DB
-const { Client } = require('pg');
-const DB_NAME = 'half-baked'
-console.log("DB_URL: ", process.env.DATABASE_URL)
-const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${ DB_NAME }`;
-const client = new Client(DB_URL);
+const { Client } = require("pg");
+const DB_NAME = "half-baked";
+console.log("DB_URL: ", process.env.DATABASE_URL);
+const DB_URL =
+  process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
+const client = new Client({
+  connectionString: DB_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
 
 // export
 module.exports = {
-  client
-}
+  client,
+};
