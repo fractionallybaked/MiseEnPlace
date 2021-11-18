cf5ca423b419d0eba741c50751ee1c2ee726dd06
Kendra <klnbianchi@gmail.com>
2021-11-17 21:17:32 -0800

    products and admin components

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,13 @@ import {
 } from "react-router-dom";
 
 import { getAllProducts } from "../api/products";
+// import { getMyID } from "../api/users";
 
 import {
   Navbar,
   SingleProduct,
-  CreateProduct
+  CreateProduct,
+  AddType,
 } from "./";
 
 const App = () => {
@@ -23,8 +25,13 @@ const App = () => {
     async function setUp() {
       try {
         const products = await getAllProducts();
-      
+
         setAllProducts(products.allProducts);
+
+        // const currentUser = await getMyID();
+        // if (isLoggedIn && currentUser.isAdmin) {
+        //   setIsAdmin(true);
+        // }
       } catch (err) {
         console.log(err);
       }
@@ -34,11 +41,11 @@ const App = () => {
 
   return (
     <div className="App">
-      <Navbar 
-      isLoggedIn={isLoggedIn}
-      setIsLoggedIn= {setIsLoggedIn}
-       />
-    
+      <Navbar
+        isLoggedIn={isLoggedIn}
+        setIsLoggedIn={setIsLoggedIn}
+      />
+
       <Switch>
         <Route path="/about">
           <h2>about</h2>
@@ -56,12 +63,19 @@ const App = () => {
           <h2>cart</h2>
         </Route>
         <Route path="/admin">
-        <CreateProduct setAllProducts={setAllProducts} />
+          {isAdmin
+            ? <CreateProduct setAllProducts={setAllProducts} />
+            : null
+          }
+          <AddType 
+          allProducts={allProducts}
+          setAllProducts={setAllProducts}
+          isAdmin={isAdmin}/>
         </Route>
         <Route exact path="/">
           <h2>home</h2>
         </Route>
-        
+
       </Switch>
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as Navbar } from './Navbar';
 export { default as SingleProduct } from './SingleProduct';
 export { default as CreateProduct } from './CreateProduct';
 export { default as EditProduct } from './EditProduct';
+export { default as DeleteProduct } from './DeleteProduct';
+export { default as AddType } from './AddType';
