8b418957ea7c1d56f7c17953b80429e364404a2d
Kendra <klnbianchi@gmail.com>
2021-11-17 19:17:40 -0800

    added nav and routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useLocation,
+} from "react-router-dom";
+
+import { getAllProducts } from "../api/products";
 
 import {
-  getSomething
-} from '../api';
-
-import {
-  Navbar
-} from './'
+  Navbar,
+  SingleProduct
+} from "./";
 
 const App = () => {
-  const [message, setMessage] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [allProducts, setAllProducts] = useState([]);
 
   useEffect(() => {
-    getSomething()
-      .then(response => {
-        setMessage(response.message);
-      })
-      .catch(error => {
-        setMessage(error.message);
-      });
-  });
+    async function setUp() {
+      try {
+        const products = await getAllProducts();
+        setAllProducts(products);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    setUp();
+  }, []);
 
   return (
     <div className="App">
       <Navbar />
-      <h1>Hello, World!</h1>
-      <h2>{ message }</h2>
+    
+      <Switch>
+        <Route path="/about">
+          <h2>about</h2>
+        </Route>
+        <Route path="/products">
+          <SingleProduct allProducts={allProducts} />
+        </Route>
+        <Route path="/login">
+          <h2>login</h2>
+        </Route>
+        <Route path="/register">
+          <h2>register</h2>
+        </Route>
+        <Route path="/cart">
+          <h2>cart</h2>
+        </Route>
+        <Route exact path="/">
+          <h2>home</h2>
+        </Route>
+        
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
