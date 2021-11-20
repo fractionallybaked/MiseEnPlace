8c82a3233557dbad80e1c73162c4a40f4650a5af
Robert Liu <robertpliu@gmail.com>
2021-11-20 11:29:13 -0800

    dotenv install

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { clearCurrentUser, getToken } from "../auth";
 
 const Navbar = ({ isLoggedIn, setIsLoggedIn, isAdmin }) => {
   return (
@@ -9,8 +10,19 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn, isAdmin }) => {
         <Link to="/about">About</Link>
         <Link to="/products">Products</Link>
         <Link to="/admin">Admin</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/login">Logout</Link>
+        {isLoggedIn ? (
+          <Link
+            to="/login"
+            onClick={() => {
+              clearCurrentUser();
+              setIsLoggedIn(false);
+            }}
+          >
+            Logout
+          </Link>
+        ) : (
+          <Link to="/login"> Sign In</Link>
+        )}
         <Link to="/register">Sign up</Link>
         <Link to="/cart">
           <span className="material-icons">shopping_cart</span>
