61e843e22f815fd56feb34bbfd9f78656e08f4d8
Kendra <klnbianchi@gmail.com>
2021-11-21 08:05:00 -0800

    commit to pull

--- src/components/AllProductsPage.js
+++ src/components/AllProductsPage.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import {SingleProduct} from './';
+import React, { useState, useEffect } from "react";
+import { SingleProduct } from './';
+import { getProductById } from '../api/products';
 
-const AllProductsPage=({allProducts, isAdmin})=>{
-return(
-    <div className= "all-products-main-container">
-        <SingleProduct
-        allProducts={allProducts}
-        isAdmin={isAdmin} />
-    </div>
-)
+const AllProductsPage = ({ allProducts, isAdmin }) => {
+    const [products, setProducts] = useState([]);
+  
+    const idArr = allProducts.map(e => e.id);
+
+    useEffect(() => {
+        async function setUp() {
+            try {
+                const prods = []
+                for (const ids of idArr) {
+                    const withType = await getProductById(ids);
+                    prods.push(withType.products)
+                }
+                setProducts([...prods])
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        setUp();
+    }, [allProducts]);
+
+    return (
+        <div className="all-products-main-container">
+              <div className="baked-goods-main-container">
+                  <h2>Baked Goods</h2>
+              <SingleProduct
+                allProducts={allProducts}
+                isAdmin={isAdmin} />
+                </div>
+            <div className="beverages-main-container">
+<h2>Beverages</h2>
+            </div>
+           
+        </div>
+    )
 }
 
 export default AllProductsPage;
