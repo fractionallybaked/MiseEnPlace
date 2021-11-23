a956e40dfc4b7bf87c62e385b50a0a35e0b76a2a
Robert Liu <robertpliu@gmail.com>
2021-11-22 19:02:13 -0800

    admin created

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-const Admin = ({}) => {};
+const Admin = ({}) => {
+  //const [productId, setProductId] = useState("");
+  return (
+    <div>
+      <h2>Administrative Functions</h2>
+      <nav>
+        <section className="nav-links">
+          <Link to="/addtype">Add Item Type</Link>
+          <Link to="/createproducts">Create Product</Link>
+          <Link to="/manageusers">User Center</Link>
+        </section>
+      </nav>
+    </div>
+  );
+};
 
 export default Admin;
