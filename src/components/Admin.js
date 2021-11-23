93b1e8973dc54ff9495c92889296851a67608748
Robert Liu <robertpliu@gmail.com>
2021-11-22 20:26:37 -0800

    admin navbar added

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { CreateProduct, AddType } from "./";
 
-const Admin = ({}) => {
-  //const [productId, setProductId] = useState("");
+const Admin = ({ allProducts, setAllProducts, isAdmin }) => {
   return (
     <div>
-      <h2>Administrative Functions</h2>
-      <nav>
+      <nav className="admin-nav">
         <section className="nav-links">
-          <Link to="/addtype">Add Item Type</Link>
-          <Link to="/createproducts">Create Product</Link>
-          <Link to="/manageusers">User Center</Link>
+          <Link to="/admin/createproduct">Create a Product</Link>
+          <Link to="/admin/addtype">Add Type</Link>
+          <Link to="/admin/manageusers">Manage Users</Link>
         </section>
       </nav>
+
+      <Switch>
+        <Route exact path="/admin/createproduct">
+          <div>
+            <CreateProduct setAllProducts={setAllProducts} isAdmin={isAdmin} />
+          </div>
+        </Route>
+
+        <Route exact path="/admin/addtype">
+          <div>
+            <AddType
+              allProducts={allProducts}
+              setAllProducts={setAllProducts}
+              isAdmin={isAdmin}
+            />
+          </div>
+        </Route>
+
+        <Route exact path="/admin/manageusers">
+          <div>
+            <h2 className="admin-header">Manage Users</h2>
+          </div>
+        </Route>
+      </Switch>
     </div>
   );
 };
