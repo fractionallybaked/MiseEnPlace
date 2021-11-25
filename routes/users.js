894f518fef35dc123574b9fcaef6ef020f5ed60d
Robert Liu <robertpliu@gmail.com>
2021-11-25 15:18:05 -0800

    admin getallusers fixed.  heroku push

--- routes/users.js
+++ routes/users.js
@@ -86,7 +86,8 @@ usersRouter.get("/", requireUser, async (req, res, next) => {
 
   try {
     const users = await getAllUsers(id);
-    return users;
+    console.log("ALL USERS ROUTE: ", users);
+    res.send(users);
   } catch (error) {
     next(error);
   }
