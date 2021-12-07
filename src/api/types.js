a62c665aa99c5aaa4aa10c8ea5fd70aa5b39f651
Robert Liu <robertpliu@gmail.com>
2021-12-06 20:11:28 -0800

    new url

--- src/api/types.js
+++ src/api/types.js
@@ -1,24 +1,24 @@
 import axios from "axios";
 import { getToken } from "../auth";
 
-const BASE = "https://polar-harbor-90312.herokuapp.com/api";
+const BASE = "https://mise-en-place-oui.herokuapp.com/api";
 
 export async function getAllTypes() {
-    try {
-        const { data } = await axios.get(`${BASE}/types`);
+  try {
+    const { data } = await axios.get(`${BASE}/types`);
 
-        return data;
-    } catch (error) {
-        throw error;
-    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
 }
 
 export async function getProductByType(typeName) {
-    try {
-        const { data } = await axios.get(`${BASE}/${typeName}/products`);
+  try {
+    const { data } = await axios.get(`${BASE}/${typeName}/products`);
 
-        return data;
-    } catch (error) {
-        throw error;
-    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
 }
--- src/api/users.js
+++ src/api/users.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken } from "../auth";
 
-const BASE = "https://polar-harbor-90312.herokuapp.com/api";
+const BASE = "https://mise-en-place-oui.herokuapp.com/api";
 
 export async function loginUser(username, password) {
   try {
