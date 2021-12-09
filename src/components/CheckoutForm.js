e790769848bec40b1e7d4304f0f08c706d5c7843
Kendra <klnbianchi@gmail.com>
2021-12-08 20:04:42 -0800

    checkout form styling

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Flex, Heading, Stack, Button, Box, InputGroup, Input, FormHelperText } from "@chakra-ui/react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const CheckoutForm = () => {
@@ -10,8 +10,6 @@ const CheckoutForm = () => {
   const [zipcode, setZip] = useState("");
 
   return (
-    <div>
-      <h2>Checkout</h2>
       <Flex
         direction="column"
         w="100wh"
@@ -20,6 +18,22 @@ const CheckoutForm = () => {
         justify="center"
         align="center"
       >
+          <Flex
+            direction="column"
+            align="center"
+            justify="center"
+            wrap="wrap"
+            mt="220px"
+          >
+        <Heading size='xl'>Checkout</Heading>
+        <Stack
+        flexDir="column"
+        mb="2"
+        justify="center"
+        align="center"
+        mt='1em'
+      >
+        <Box minW={{ base: "90%", md: "468px" }}>
         <form
           id="checkout"
           onSubmit={(event) => {
@@ -32,63 +46,80 @@ const CheckoutForm = () => {
             }
           }}
         >
-          <fieldset>
-            <label>Name</label>
-            <input
+           <Stack
+              spacing={4}
+              p="1rem"
+              backgroundColor="whiteAlpha.900"
+              boxShadow="md"
+              mb='1rem'
+            >
+          <InputGroup>
+            <Input
               id="name"
               type="text"
-              placeholder="First and Last"
+              placeholder="Name"
               value={name}
               onChange={(event) => {
                 setName(event.target.value);
               }}
               required
             />
-          </fieldset>
+          </InputGroup>
 
-          <fieldset>
-            <label>Address</label>
-            <input
+          <InputGroup>
+            <Input
               id="address"
               type="text"
-              placeholder="Street name and number"
+              placeholder="Address"
               value={address}
               onChange={(event) => {
                 setAddress(event.target.value);
               }}
               required
             />
-          </fieldset>
+          </InputGroup>
 
-          <fieldset>
-            <label>Zipcode</label>
-            <input
+          <InputGroup>
+            <Input
               id="zip"
               type="text"
               value={zipcode}
+              placeholder="Zipcode"
               onChange={(event) => {
                 setZip(event.target.value);
               }}
               required
             />
-          </fieldset>
+          </InputGroup>
 
-          <fieldset>
-            <label>City</label>
-            <input
+          <InputGroup>
+            <Input
               id="city"
               type="text"
+              placeholder="City"
               value={city}
               onChange={(event) => {
                 setCity(event.target.value);
               }}
               required
             />
-          </fieldset>
-          <button type="submit">Complete Checkout</button>
+          </InputGroup>
+          </Stack>
+          <Button
+                borderRadius={0}
+                type="submit"
+                variant="solid"
+                bg="#c97c5d"
+                width="full"
+              >
+                Complete Checkout
+              </Button>
+          
         </form>
+        </Box>
+        </Stack>
       </Flex>
-    </div>
+      </Flex>
   );
 };
 
