1568675940c00c3e79734b5ec13b218fc9002ea0
Emelie M <emelie.moya@aol.com>
2021-12-08 18:40:40 -0800

    started checkout form

---
+++ src/components/CheckoutForm.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import {Flex, Heading, Text} from '@chakra-ui/react';
+
+const CheckoutForm = () => {
+  const [name, setName] = useState("")
+  const [address, setAddress] = useState("")
+  const []
+
+};
+
+export default CheckoutForm;
